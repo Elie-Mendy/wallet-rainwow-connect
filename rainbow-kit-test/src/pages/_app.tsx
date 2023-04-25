f2b455ac4fe15e5ad2e8fa13907e55633251e126
Elie MENDY <emendy@efficity.com>
2023-04-25 09:11:26 +0200

    implement rainbow connection

---
+++ rainbow-kit-test/src/pages/_app.tsx
@@ -0,0 +1,44 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum ,goerli, hardhat } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  //[mainnet, polygon, optimism, arbitrum],
+  [goerli, hardhat],
+  [
+    //alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider theme={darkTheme()} chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
+}
